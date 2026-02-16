4179669eabb781719ccc10746c17d25dd02c9230
Javier Montero Chaves <monterodaw@gmail.com>
2026-02-16 10:57:34 +0100

    Inicio proyecto Practica 4

--- src/com/ejemplo/p4/Main.java
+++ src/com/ejemplo/p4/Main.java
@@ -1,5 +1,8 @@
-package com.ejemplo.p4;
-
-public class Main {
-
+package com.ejemplo.p4; 
+public class Main { 
+  public static void main(String[] args) {
+    System.out.println("Practica 4 - Git casos reales");
+    System.out.println("Javier Montero Chaves");
+    System.out.println("Esto es un cambio malo");
+  } 
 }
