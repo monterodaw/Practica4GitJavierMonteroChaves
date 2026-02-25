2330b0b0130e2425d30519eef7b29ad1e22d0fde
Javier Montero Chaves <monterodaw@gmail.com>
2026-02-25 12:49:18 +0100

    Actualizar mensajes de salida en Main

--- src/com/ejemplo/p4/Main.java
+++ src/com/ejemplo/p4/Main.java
@@ -3,6 +3,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Practica 4 - Git casos reales");
     System.out.println("Javier Montero Chaves");
-    System.out.println("Esto es un cambio malo");
+    System.out.println("Nueva línea de prueba. Punto 4.1");
   } 
 }
