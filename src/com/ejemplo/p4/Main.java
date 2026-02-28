349b0dda936718d03f6af09705489e5af7428591
Javier Montero Chaves <monterodaw@gmail.com>
2026-02-28 13:07:34 +0100

    Commit para perder y recuperar

--- src/com/ejemplo/p4/Main.java
+++ src/com/ejemplo/p4/Main.java
@@ -3,6 +3,6 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Practica 4 - Git casos reales");
     System.out.println("Javier Montero Chaves");
-    System.out.println("Nueva línea de prueba. Punto 4.1");
+    System.out.println("Mensaje añadido en el punto 9 - REFLOG");
   } 
 }
